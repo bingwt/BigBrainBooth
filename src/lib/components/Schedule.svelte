df88f256d392f5753a2845d3a435b381ed0bf2e9
Bing Wen Tan <bing@bwtmail.com>
2024-10-05 21:44:36 +0800

    Added draggable booking slot creation

--- src/lib/components/Schedule.svelte
+++ src/lib/components/Schedule.svelte
@@ -10,32 +10,41 @@
 	let calendar: any;
 	
 	onMount(() => {
-	calendar = new Calendar({
-    target: document.getElementById('ec'),
-    props: {
-        plugins: [TimeGrid, Interaction],
-        options: {
-            view: 'timeGridWeek',
-			dateClick: (event: any) => {
-			const booking = {
-				title: email?.split('@')[0],
-				start: event.date
+		calendar = new Calendar({
+		target: document.getElementById('ec'),
+		props: {
+			plugins: [TimeGrid, Interaction],
+			options: {
+				view: 'timeGridWeek',
+				nowIndicator: true,
+				selectable: true,
+				slotEventOverlap: true,
+				eventClick: (event: any) => {
+					if (event.event.title === login)
+					calendar.removeEventById(event.event.id);
+				},
+				select: (info: any) => {
+					let overlap: boolean = false;
+					const booking = {
+						title: email?.split('@')[0],
+						start: info.start,
+						end: info.end
+					}
+					const booked = calendar.getEvents();
+					for (let i: number = 0; i < booked.length; i++) {
+						if (booking.start > booked[i].start && booking.start < booked[i].end)
+							overlap = true;
+						if (booking.end > booked[i].start && booking.end < booked[i].end)
+							overlap = true;
+					}
+					console.log(overlap);
+					if (!overlap)
+						addBooking(booking);
+				},
+				events: []
 			}
-			// console.log(booking);
-			addBooking(booking);
-		},
-		eventMouseEnter: (event: any) => {
-			console.log(event);
-		},
-		eventClick: (event: any) => {
-			if (event.event.title === login)
-				calendar.removeEventById(event.event.id);
-			console.log(event);
-		},
-            events: []
-        }
-    }
-});
+		}
+	});
 });
 
 function addBooking(booking: any) {
