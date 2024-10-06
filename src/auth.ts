c46c9654a69a3ad408ca4cd937cd5c894e710310
Bing Wen Tan <bing@bwtmail.com>
2024-10-06 21:52:39 +0800

    Removed Auth.js

--- src/auth.ts
+++
@@ -1,7 +0,0 @@
-import { SvelteKitAuth } from "@auth/sveltekit"
-import auth42 from "@auth/sveltekit/providers/42-school"
- 
-export const { handle, signIn } = SvelteKitAuth({
-  providers: [auth42],
-  trustHost: true
-}) 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import { SignIn } from "@auth/sveltekit/components"
 	import { page } from "$app/stores";
 	/** @type {import('./$types').PageData} */
 	export let data: any;
@@ -39,13 +38,5 @@
 	<a href="/booking" class="btn btn-secondary text-primary hover:btn-accent hover:text-primary font-satoshi font-bold p-4 h-20">
 		<p>View Schedule</p>
 	</a>
-	<!-- <button class="btn btn-secondary text-primary hover:btn-accent hover:text-primary font-satoshi font-bold p-4 h-20">
-		<SignIn provider="42-school" signInPage="signin" className="w-full">
-			<div slot="submitButton" class="flex flex-col place-items-center justify-center">
-				<p>Sign in with</p>
-			<svg class="w-8 h-8" role="graphics-symbol" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><title>42</title><path d="M19.581 16.851H24v-4.439ZM24 3.574h-4.419v4.42l-4.419 4.418v4.44h4.419v-4.44L24 7.993Zm-4.419 0h-4.419v4.42zm-6.324 8.838H4.419l8.838-8.838H8.838L0 12.412v3.595h8.838v4.419h4.419z"/></svg>
-			</div>
-		</SignIn>
-	</button> -->
 </div>
 {/if}
