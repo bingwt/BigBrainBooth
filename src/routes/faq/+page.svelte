8892154c6e6622486f7aea3def87463c98df185f
Bing Wen Tan <bing@bwtmail.com>
2024-10-11 01:56:42 +0800

    Updated faq page

--- src/routes/faq/+page.svelte
+++ src/routes/faq/+page.svelte
@@ -26,22 +26,28 @@
 
 
 <h1 class="text-4xl font-bold">FAQ</h1>
-<div class="overflow-y-scroll p-4 flex flex-col gap-8">
+<div class="overflow-y-scroll p-4 flex flex-col gap-8 text-justify">
 	<div>
-		<h2 class="text-2xl font-bold">What's BBB for? 🤔</h2>
-		<li class="text-justify">It's a dedicated space for students who would like to step-up and offer help on a specific project e.g. so_long or pipex etc. We've noticed that there are some who might be struggling with their projects but are unsure if the peers on site are available for help, so we are trying BBB out to further help you all along with your challenging projects.</li>
+		<h2 class="text-2xl font-bold">What's BBB for?</h2>
+		<li>It's a dedicated space for students who would like to step-up and offer help on a specific project</li>
+		<li>We've noticed that there are some who might be struggling with their projects.</li>
+		<li>We are trying BBB out to further help you all along with your challenging projects.</li>
 	</div>
 	<div>
-		<h2 class="text-2xl font-bold">How can I be a part of BBB? :homer_thinking:</h2>
-		<li class="text-justify">Simply fill out the roster that will be stuck on the wall beside the workstation. This roster will be student managed so only fill out the times and projects you are open to helping. Please allow for different peers to try out BBB and not book the space out for more than 4hrs at a time (this timing can be negotiated in future if we see fit as this is still after all, a trial Bocal project).</li>
+		<h2 class="text-2xl font-bold">How can I be a part of BBB?</h2>
+		<li>Simply <a href="/booking" class="font-bold underline">reserve</a> your slot at the BBB.</li>
+		<li>This will be student managed, only fill out the times and projects you are open to helping.</li>
+		<li>Please allow for different peers to try out BBB.</li>
+		<li>Do not book the space for more than <span class="font-bold">4 hours</span> at a time!</li>
+		<li>This timing can be <a href="/feedback" class="font-bold underline">negotiated</a> in future if we see fit as this is still after all, a trial Bocal project.</li>
 	</div>
 	<div>
-		<h2 class="text-2xl font-bold">Will there be a reward for students that help out at BBB? :awkward_monkey:</h2>
-		<li class="text-justify">Yes! The awesome feeling and knowledge of helping a fellow peer in need is the best reward money can't buy :fry_money:</li>
+		<h2 class="text-2xl font-bold">Will there be a reward for students that help out at BBB?</h2>
+		<li>Yes! The awesome feeling and knowledge of helping a fellow peer in need is the best reward money can't buy!</li>
 	</div>
 	<div>
-		<h2 class="text-2xl font-bold">Can we suggest improvements on BBB? :face_in_clouds:</h2>
-		<li class="text-justify">Definitely! Drop us some <a href="/feedback" class="font-bold underline">feedback</a>!</li>
+		<h2 class="text-2xl font-bold">Can we suggest improvements on BBB?</h2>
+		<li>Definitely! Drop us some <a href="/feedback" class="font-bold underline">feedback</a>!</li>
 	</div>
 	<!-- <h1 class="text-2xl font-bold text-center">Happy BBB-ing everyone! Hope to see the space being utilized by everyone :success:</h1> -->
 </div>
