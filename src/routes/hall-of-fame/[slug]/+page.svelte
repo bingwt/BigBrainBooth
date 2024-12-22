bf1ef4d2934009216bdcca509fdf5c5c115dd374
Bing Wen Tan <bing@bwtmail.com>
2024-12-23 01:52:37 +0800

    Added voting, sharing and saving to post page

--- src/routes/hall-of-fame/[slug]/+page.svelte
+++ src/routes/hall-of-fame/[slug]/+page.svelte
@@ -6,6 +6,8 @@
     let post;
 
     let login = $page.data?.user?.login;
+    let saved = false;
+    let buttonText = "share";
 
     onMount(async () => {
         const response = await fetch(`/api/v1/list/hall-of-fame`, {
@@ -13,6 +15,7 @@
             body: JSON.stringify({ id: $page.params.slug }),
         });
         post = await response.json();
+        saved = post.saves.includes(login) ? true : false;
     });
 
     function isVideo(media) {
@@ -82,6 +85,45 @@
     function sortComments(comments) {
         return comments.sort((a, b) => new Date(b.date) - new Date(a.date));
     }
+
+    async function submitSaved() {
+        let newSaved;
+        if (saved) {
+            newSaved = post.saves.filter((save) => save !== login);
+        } else {
+            newSaved = [...post.saves, login];
+        }
+
+        const updatedRecord = {
+            saves: newSaved,
+        };
+
+        await fetch(`/api/v1/update/hall-of-fame`, {
+            method: "POST",
+            body: JSON.stringify({
+                id: post.id,
+                record: updatedRecord,
+            }),
+        });
+
+        const response = await fetch(`/api/v1/list/hall-of-fame`, {
+            method: "POST",
+            body: JSON.stringify({ id: post.id }),
+        });
+        post = await response.json();
+
+        saved = post.saves.includes(login) ? true : false;
+    }
+
+    function copyToClipboard() {
+        const url = `${window.location.origin}/hall-of-fame/${post.id}`;
+        navigator.clipboard.writeText(url).then(() => {
+            buttonText = "copied to clipboard!";
+            setTimeout(() => {
+                buttonText = "share";
+            }, 2000);
+        })
+    }
 </script>
 
 <svelte:head>
@@ -90,7 +132,7 @@
 
 {#if post}
     <div class="hero-content">
-        <div class="flex flex-col align-middle gap-4 w-screen">
+        <div class="flex flex-col align-middle gap-4 w-screen mt-8">
             <h1 class="text-4xl font-bold">{post.title}</h1>
             <div class="flex flex-row gap-2">
                 {#each post.tags as tag}
@@ -107,6 +149,53 @@
                     >{post.author.login}</a
                 >
             </p>
+            <div class="flex w-full h-4 items-center">
+                <!-- <a
+                    href={`/hall-of-fame/${post.id}`}
+                    class="btn btn-link text-secondary font-bold hover:text-accent hover:underline no-underline p-0"
+                >
+                    {post.comments.length}
+                    {post.comments.length === 1 ? "comment" : "comments"}
+                </a> -->
+                <VoteButton {post} type="vertical" />
+                <div class="divider divider-horizontal divider-secondary"></div>
+                <button
+                    class="btn btn-link text-secondary font-bold hover:text-accent hover:underline no-underline p-0"
+                    on:click={copyToClipboard}
+                >
+                    {buttonText}
+                </button>
+                <div class="divider divider-horizontal divider-secondary"></div>
+                <button
+                    class="btn btn-link text-secondary font-bold hover:text-error hover:underline no-underline p-0 hover:scale-[1.2] transition-all duration-300"
+                    on:click={submitSaved}
+                >
+                    {#if saved}
+                        <svg
+                            class="text-error"
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="20"
+                            height="20"
+                            fill="currentColor"
+                            viewBox="0 0 256 256"
+                            ><path
+                                d="M240,102c0,70-103.79,126.66-108.21,129a8,8,0,0,1-7.58,0C119.79,228.66,16,172,16,102A62.07,62.07,0,0,1,78,40c20.65,0,38.73,8.88,50,23.89C139.27,48.88,157.35,40,178,40A62.07,62.07,0,0,1,240,102Z"
+                            ></path></svg
+                        >
+                    {:else}
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="20"
+                            height="20"
+                            fill="currentColor"
+                            viewBox="0 0 256 256"
+                            ><path
+                                d="M178,36c-20.09,0-37.92,7.93-50,21.56C115.92,43.93,98.09,36,78,36a66.08,66.08,0,0,0-66,66c0,72.34,105.81,130.14,110.31,132.57a12,12,0,0,0,11.38,0C138.19,232.14,244,174.34,244,102A66.08,66.08,0,0,0,178,36Zm-5.49,142.36A328.69,328.69,0,0,1,128,210.16a328.69,328.69,0,0,1-44.51-31.8C61.82,159.77,36,131.42,36,102A42,42,0,0,1,78,60c17.8,0,32.7,9.4,38.89,24.54a12,12,0,0,0,22.22,0C145.3,69.4,160.2,60,178,60a42,42,0,0,1,42,42C220,131.42,194.18,159.77,172.51,178.36Z"
+                            ></path></svg
+                        >
+                    {/if}
+                </button>
+            </div>
             <div
                 class="overflow-y-scroll p-4 flex flex-col gap-8 text-left h-[75dvh] fade-top pt-14"
             >
