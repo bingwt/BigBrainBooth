2ab4b1008e38d3ffdf37a3b65857ce6bc01a4e4e
Bing Wen Tan <bing@bwtmail.com>
2024-12-23 18:10:06 +0800

    Updated create post button to use goto

--- src/routes/hall-of-fame/+page.svelte
+++ src/routes/hall-of-fame/+page.svelte
@@ -1,5 +1,6 @@
 <script>
     import { onMount } from "svelte";
+    import { goto } from "$app/navigation";
     import UserPost from "$lib/components/UserPost.svelte";
 
     let posts = [];
@@ -25,8 +26,8 @@
             {/each}
         </div>
         <div class="toast">
-            <a
-                href="/hall-of-fame/create"
+            <button
+                on:click={() => goto("/hall-of-fame/create")}
                 class="btn btn-ghost btn-circle btn-lg text-secondary hover:text-accent hover:scale-[1.2] transition-all duration-300"
             >
                 <svg
@@ -39,7 +40,7 @@
                         d="M232.49,55.51l-32-32a12,12,0,0,0-17,0l-96,96A12,12,0,0,0,84,128v32a12,12,0,0,0,12,12h32a12,12,0,0,0,8.49-3.51l96-96A12,12,0,0,0,232.49,55.51ZM192,49l15,15L196,75,181,60Zm-69,99H108V133l56-56,15,15Zm105-7.43V208a20,20,0,0,1-20,20H48a20,20,0,0,1-20-20V48A20,20,0,0,1,48,28h67.43a12,12,0,0,1,0,24H52V204H204V140.57a12,12,0,0,1,24,0Z"
                     ></path></svg
                 >
-            </a>
+            </button>
         </div>
     </div>
 </div>
