e160db83d578b74de5f575a39180f9e58c84a185
Bing Wen Tan <bing@bwtmail.com>
2024-12-27 02:56:34 +0800

    Removed duplicate import in upload endpoint

--- src/routes/api/v1/upload/+server.ts
+++ src/routes/api/v1/upload/+server.ts
@@ -3,7 +3,6 @@ import { error, json } from '@sveltejs/kit';
 import { S3 } from '$lib/s3';
 import { PutObjectCommand } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
-import { json } from '@sveltejs/kit';
 import { v4 as uuidv4 } from 'uuid';
 
 const slugifyString = (str: string) => {
